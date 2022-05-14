dcd45fa64915b7d06f7142a59331f4118ba4447c
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-14 13:04:05 -0500

    debugging

--- routes/notes.js
+++ routes/notes.js
@@ -7,7 +7,7 @@ const {
 } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
-notes.get('/notes', (req, res) => {
+notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
@@ -44,12 +44,12 @@ notes.delete('/:note_id', (req, res) => {
 });
 
 // POST Route for a new note
-notes.post('/notes', (req, res) => {
+notes.post('/', (req, res) => {
   console.log(req.body);
   //destructuring the items in req.body
   const { title, text } = req.body;
 
-  if (title && text) {
+  if (req.body) {
     //variable for the object will be saved
     const newNote = {
       title,
