a590194ae2b8853707e00f3ea08e89f353e3affa
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-14 00:03:02 -0500

    fixing routes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for notes.html
-app.get('/notes', (req, res) =>
+app.get('/api/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
