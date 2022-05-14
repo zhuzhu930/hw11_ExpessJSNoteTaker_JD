74d1b78d5e5c765fae691cda473162100196bf6b
Jessie Doherty <zhuzhu930@yahoo.com>
2022-05-14 08:53:01 -0500

    fixing notes.js

--- routes/notes.js
+++ routes/notes.js
@@ -11,6 +11,32 @@ notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// POST Route for a new note
+notes.post('/', (req, res) => {
+  // console.log(req.body);
+  //destructuring the items in req.body
+  const { noteTitle, noteText } = req.body;
+
+  if (noteTitle && noteText) {
+    const newNote = {
+      noteTitle,
+      noteText,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    
+    const response = {
+      status: "Success", 
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.error('Error in adding a note.');
+  }
+});
+
 // GET Route for a specific note
 notes.get('/:note_id', (req, res) => {
   const noteId = req.params.note_id;
@@ -41,30 +67,6 @@ notes.delete('/:note_id', (req, res) => {
     });
 });
 
-// POST Route for a new note
-notes.post('/', (req, res) => {
-  // console.log(req.body);
-  //destructuring the items in req.body
-  const { noteTitle, noteText } = req.body;
 
-  if (req.body) {
-    const newNote = {
-      noteTitle,
-      noteText,
-      note_id: uuidv4(),
-    };
-
-    readAndAppend(newNote, './db/db.json');
-    
-    const response = {
-      status: "Success", 
-      body: newNote,
-    };
-
-    res.json(response);
-  } else {
-    res.error('Error in adding a note.');
-  }
-});
 
 module.exports = notes;
